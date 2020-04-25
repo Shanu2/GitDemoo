400895a2bed4a2e7f5492a861487155ee4111913
Shanu2 <ayu.verma0212@gmail.com>
2020-04-25 15:08:14 +0530

    second time commit of maven project

--- src/test/java/MavenProgram/SeleniumTest.java
+++ src/test/java/MavenProgram/SeleniumTest.java
@@ -7,10 +7,19 @@ public class SeleniumTest {
 @Test
 public void SeleniumTest()
 {
+	
 	System.out.println("Welcome Selenium");
 }
 @Test
 public void SeleniumTest2() {
 	System.out.println("Welcome Selenium Testing 2");
+}
+@Test
+public void SeleniumTestGit()
+{
+	
+	System.out.println("Welcome Selenium git");
+	System.out.println("Welcome Selenium git1");
+	System.out.println("Welcome Selenium git2");
 }
 }
